babc03b3ad121e22bf4261f4f88516cbda83172d
Jaybee <jubril_tunde@yahoo.com>
2023-04-07 10:17:37 +0100

    add transaction form UI

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,16 @@
 import styles from "./Home.module.css"
 import React from 'react'
+import TransactionForm from "./TransactionForm"
 
 const Home = () => {
   return (
-    <div>
-        Home
+    <div className={styles.container}>
+        <div className={styles.content}>
+            transactions list
+        </div>
+        <div className={styles.sidebar}>
+            <TransactionForm />
+        </div>
     </div>
   )
 }
